5c09889da52e064588486904411b7ea7a99ebe59
DanielQuanz1 <alfarizidaniel5@gmail.com>
2025-05-22 15:48:13 +0700

    PERTEMUAN 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 // import java.io;
 
 // Syste.out.println("Hello World");
@@ -59,3 +63,16 @@ Route::get('pertemuan7/js', function () {
 Route::get('ets', function () {
 	return view('index');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+//route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
