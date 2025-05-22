842dd26499b9aa28daa28db2bf3cef23327955e1
DanielQuanz1 <alfarizidaniel5@gmail.com>
2025-05-22 10:27:41 +0700

    PR PERTEMUAN 11

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,46 @@ Route::get('blog', function () {
 	return view('blog');
 });
 
-Route::get('pertama', function () {
+Route::get('frontend', function () {
+    return view('frontend');
+});
+
+Route::get('pertemuan1', function () {
 	return view('pertama');
+});
+
+Route::get('pertemuan3/Forms', function () {
+	return view('Forms');
+});
+
+Route::get('pertemuan3/Transition', function () {
+	return view('Transition');
+});
+
+Route::get('pertemuan3/bootstrap1', function () {
+	return view('bootstrap1');
+});
+
+Route::get('pertemuan3/template1', function () {
+	return view('template1');
+});
+
+Route::get('pertemuan4', function () {
+	return view('latsol');
+});
+
+Route::get('linktree', function () {
+	return view('linktree');
+});
+
+Route::get('pertemuan7/validasi', function () {
+	return view('validasi1');
+});
+
+Route::get('pertemuan7/js', function () {
+	return view('js1');
+});
+
+Route::get('ets', function () {
+	return view('index');
 });
