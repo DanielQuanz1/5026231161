f56ed61b8ede0664f1a78d4649db3b66f5d090e3
DanielQuanz1 <alfarizidaniel5@gmail.com>
2025-06-05 16:14:00 +0700

    PERTEMUAN 14

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,278 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Bank Perekonomian Rakyat Insumo Sumberarto</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets\img\iconbpr.png" />
-        <!-- Bootstrap Icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
-        <!-- SimpleLightbox plugin CSS-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{asset('styles.css')}}"rel="stylesheet" />
+@include('template')
 
-    </head>
-    <body id="page-top">
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="#page-top">BPR INSUMO SUMBERARTO</a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#about">Tentang</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#services">Produk</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio">Info</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#contact">Kontak</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <!-- Masthead-->
-        <header class="masthead">
-            <div class="container px-4 px-lg-5 h-100">
-                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
-                    <div class="col-lg-8 align-self-end">
-                        <h1 class="text-white font-weight-bold">"BISA"</h1>
-                        <hr class="divider" />
-                        <h1 class="text-white font-weight-bold">BANK PEREKONOMIAN RAKYAT INSUMO SUMBERARTO</h1>
-                        <hr class="divider" />
-                    </div>
-                    <div class="col-lg-8 align-self-baseline">
-                        <p class="text-white-75 mb-5">Mitra Usaha Andalan Anda dengan Komitmen Pelayanan Terbaik Setulus Hati Menuju Sukses!</p>
-                        <a class="btn btn-primary btn-  xl" href="#about">Lebih Lanjut</a>
-                    </div>
-                </div>
-            </div>
-        </header>
-        <!-- About-->
-        <section class="page-section bg-primary" id="about">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8 text-center">
-                        <h2 class="text-white mt-0">Kami Siap Melayani Anda!</h2>
-                        <hr class="divider divider-light" />
-                        <p class="text-white-75 mb-4">Menyediakan Layanan Kredit yang cepat dan mudah, Tabungan yang Aman dan menguntungkan, hingga Deposito agar saldo anda terus bertambah!</p>
-                        <a class="btn btn-light btn-xl" href="#services">Mulai Segera!</a>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Services-->
-        <section class="page-section" id="services">
-            <div class="container px-4 px-lg-5">
-                <h2 class="text-center mt-0">PRODUK KAMI</h2>
-                <hr class="divider" />
-                <div class="row gx-4 gx-lg-5">
-                    <div class="col-lg-4 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
-                            <h3 class="h4 mb-2">KREDIT</h3>
-                            <p class="text-muted mb-0">Dapatkan akses cepat & mudah untuk kebutuhan dana yang Anda butuhkan. Ajukan kredit anda sekarang!</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
-                            <h3 class="h4 mb-2">Deposito</h3>
-                            <p class="text-muted mb-0"> Saldo Anda terus bertambah meskipun anda sedang bersantai. Buka akun deposito Anda segera tanpa perlu datang ke kantor.</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 text-center">
-                        <div class="mt-5">
-                            <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
-                            <h3 class="h4 mb-2">Tabungan</h3>
-                            <p class="text-muted mb-0">Aman & menguntungkan. Nikmati imbal hasil tinggi dengan suku bunga menjanjikan serta keamanan Dana yang terjamin oleh LPS.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Portfolio-->
-        <div id="portfolio">
-            <div class="container-fluid p-0">
-                <div class="row g-0">
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/bank1.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/bank1.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Produk</div>
-                                <div class="project-name">Kredit</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/laptop2.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/laptop2.jpg" alt="..." />
-                            <div class="portfolio-box-caption">
-                                <div class="project-category text-white-50">Produk</div>
-                                <div class="project-name">Deposito</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-lg-4 col-sm-6">
-                        <a class="portfolio-box" href="assets/img/bank6.jpg" title="Project Name">
-                            <img class="img-fluid" src="assets/img/bank6.jpg" alt="..." />
-                            <div class="portfolio-box-caption p-3">
-                                <div class="project-category text-white-50">Produk</div>
-                                <div class="project-name">Tabungan</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Call to action-->
-        <section class="page-section bg-dark text-white">
-            <div class="container px-4 px-lg-5 text-center">
-             </div>
-        </section>
-        <!-- Contact-->
+@section('content')
+<h3>Data Pegawai</h3>
 
-        <script>
-            function validasi() {
-              var nama = document.getElementById("nama").value;
-              var nomor = document.getElementById("nomor").value;
-              var email = document.getElementById("email").value;
-              var message = document.getElementById("message").value;
-
-              if(nama.length == 0 && nomor == 0 && email == 0 && message == 0){
-                Swal.fire({
-                    title: "Kesalahan Input",
-                    text: "Formulir tidak boleh kosong",
-                    icon: "error"
-                });
-                return false;
-              }
-
-              if(nama.length == 0){
-                Swal.fire({
-                    title: "Kesalahan Input",
-                    text: "Nama harus diisi",
-                    icon: "error"
-                });
-                return false;
-              }
-
-              if(nomor.length == 0){
-                Swal.fire({
-                    title: "Kesalahan Input",
-                    text: "Nomor telepon harus diisi",
-                    icon: "error"
-                });
-                return false;
-              }
-
-              if(message.length > 250){
-                Swal.fire({
-                    title: "Kesalahan Input",
-                    text: "Pesan maksimal 250 karakter",
-                    icon: "error"
-                });
-                return false;
-              }
-            }
-          </script>
-
-        <section class="page-section" id="contact">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-8 col-xl-6 text-center">
-                        <h2 class="mt-0">Kontak Kami</h2>
-                        <hr class="divider" />
-                        <p class="text-muted mb-5">Untuk Informasi lebih lanjut atau terdapat pertanyaan, hubungi kami dengan mengisi Formulir Berikut</p>
-                    </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
-                    <div class="col-lg-6">
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- * * SB Forms Contact Form * *-->
-                        <!-- * * * * * * * * * * * * * * *-->
-                        <!-- This form is pre-integrated with SB Forms.-->
-                        <!-- To make this form functional, sign up at-->
-                        <!-- https://startbootstrap.com/solution/contact-forms-->
-                        <!-- to get an API token!-->
-                        <form id="contactForm" onsubmit="return validasi();" method="post" autocomplete="off">
-                            <!-- Name input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="nama" type="text" placeholder="Enter your name..." />
-                                <label for="name">Nama Anda</label>
-
-                            </div>
-                            <!-- Email address input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="email" type="email" placeholder="name@example.com" />
-                                <label for="email">Email</label>
-
-                            </div>
-                            <!-- Phone number input-->
-                            <div class="form-floating mb-3">
-                                <input class="form-control" id="nomor" type="tel" placeholder="(123) 456-7890"  />
-                                <label for="phone">No. Telepon</label>
-                            </div>
-                            <!-- Message input-->
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem"></textarea>
-                                <label for="message">Pesan</label>
-                            </div>
-                            <!-- Submit success message-->
-                            <!---->
-                            <!-- This is what your users will see when the form-->
-                            <!-- has successfully submitted-->
-                            <div class="d-none" id="submitSuccessMessage">
-                                <div class="text-center mb-3">
-                                    <div class="fw-bolder">Form submission successful!</div>
-                                    To activate this form, sign up at
-                                    <br />
-                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                </div>
-                            </div>
-                            <!-- Submit error message-->
-                            <!---->
-                            <!-- This is what your users will see when there is-->
-                            <!-- an error submitting the form-->
-                            <!-- Submit Button-->
-                            <div class="col-md-12 text-center">
-                                <button type="submit" class="btn btn-light btn-xl">Kirim Pesan</button>
-                              </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
-                        <i class="bi-phone fs-2 mb-3 text-muted"></i>
-                        <div>+62 823 3326 1651</div>
-                    </div>
-                </div>
-            </div>
-        </section>
+<a href="/pegawai/tambah" class="btn btn-info"> + Tambah Pegawai Baru</a>
+    <br />
+    <br />
 
 
+	<form action="/pegawai/cari" method="GET" class="form-inline">
+        <label class="form -label"> Cari Data Pegawai :</label>
+        <input type="text" name="cari" placeholder="Cari Pegawai .." value="{{ old('cari') }}">
+		<input type="submit" value="CARI" class="btn btn-primary">
+	</form>
 
-        <!-- Footer-->
-        <footer class="bg-light py-5">
-            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2023 - Company Name</div></div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- SimpleLightbox plugin JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
-        <!-- SweetAlert2 -->
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </body>
+	<br/>
+
+    <table class="table table-striped">
+
+        <tr>
+            <th>Nama</th>
+            <th>Jabatan</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+            <th>Opsi</th>
+        </tr>
+        @foreach ($pegawai as $p)
+            <tr>
+                <td>{{ $p->pegawai_nama }}</td>
+                <td>{{ $p->pegawai_jabatan }}</td>
+                <td>{{ $p->pegawai_umur }}</td>
+                <td>{{ $p->pegawai_alamat }}</td>
+                <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-success">Edit</a>
+                    |
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+
+</body>
 
 </html>
